dde250b0d9067f8281557247654f401ef31196e9
LeeuCode <41273499+LeeuCode@users.noreply.github.com>
2020-11-15 12:37:46 +0200

    update script to show lightbox and add checkout config

--- includes/checkout-fields.php
+++ includes/checkout-fields.php
@@ -23,51 +23,57 @@ curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
 $resp = curl_exec($curl);
 curl_close($curl);
-// var_dump($resp);
+$result = json_decode($resp);
+// var_dump($result->session->id);
 ?>
 <script defer src="https://test-nbe.gateway.mastercard.com/checkout/version/57/checkout.js"
-        data-error="errorCallback"
+        data-error="?errorCallback"
         data-complete="?result=completeCallbackElnady"
         data-cancel="?result=cancelCallbackElnady">
 </script>
-<script defer >
-    window.onload = function(){
-    document.getElementById('showLightbox').addEventListener('click', function() {
-        var test = Checkout.showLightbox();
-        console.log(test);
-    });
-    function errorCallback(error) {
+<script async>
+    // window.addEventListener("DOMContentLoaded", function(){
+    // Your code
+    setTimeout(function() {
+        document.getElementById('showLightbox').addEventListener('click', function() {
+            var test = Checkout.showLightbox();
+            console.log(test);
+        });
+        document.getElementsByName("checkout")[0].addEventListener('submit', function(e) {
+            e.preventDefault();
+            alert('yes');
+            return false;
+        });
+        function errorCallback(error) {
             console.log(JSON.stringify(error));
-    }
-    function cancelCallback() {
+        }
+        function cancelCallback() {
             console.log('Payment cancelled');
-    }
-    Checkout.configure( {
-        merchant: 'EGPTEST1',
-        order: {
-            amount: function() {
-                //Dynamic calculation of amount
-                return 35 + 0;
+        }
+        Checkout.configure( {
+            merchant: 'EGPTEST1',
+            order: {
+                amount: '<?php echo WC()->cart->total; ?>',
+                currency: '<?php echo get_option('woocommerce_currency'); ?>',
+                description: 'Ordered goods',
+                id: '<?php echo $order_id; ?>'
             },
-            currency: 'EGP',
-            description: 'Ordered goods',
-            id: '88'
-        },
-        session:{
-            id: 'SESSION0002296876940I1471431M01'
-        },
-        interaction: {
-            operation: 'PURCHASE',
-            merchant: {
-                name: 'NBE Test',
-                address: {
-                    line1: '200 Sample St',
-                    line2: '1234 Example Town'
+            session:{
+                id: '<?php echo $result->session->id; ?>'
+            },
+            interaction: {
+                operation: 'PURCHASE',
+                merchant: {
+                    name: 'NBE Test',
+                    address: {
+                        line1: '200 Sample St',
+                        line2: '1234 Example Town'
+                    }
                 }
             }
-        }
-    });
-    }
+        });
+    }, 500)
+    // });
 </script>
 ...
 <input id="showLightbox" type="button" value="Pay with Lightbox" />
